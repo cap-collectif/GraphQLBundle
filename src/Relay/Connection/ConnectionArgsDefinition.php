b8f81fe4d3f80d91536581a392c8e168a0a0c047
Spyl <aurelien@cap-collectif.com>
2018-10-05 17:57:01 +0200

    Update ConnectionArgsDefinition.php

--- src/Relay/Connection/ConnectionArgsDefinition.php
+++ src/Relay/Connection/ConnectionArgsDefinition.php
@@ -18,15 +18,20 @@ final class ConnectionArgsDefinition implements MappingInterface
         return [
             'after' => [
                 'type' => 'String',
+                'description' => 'Returns the elements in the list that come after the specified cursor.',
             ],
             'first' => [
                 'type' => 'Int',
+                'description' => 'Returns the first `n` elements from the list.',
+                'defaultValue' => 100
             ],
             'before' => [
                 'type' => 'String',
+                'description' => 'Returns the elements in the list that come before the specified cursor.'
             ],
             'last' => [
                 'type' => 'Int',
+                'description' => 'Returns the last `n` elements from the list.'
             ],
         ];
     }
